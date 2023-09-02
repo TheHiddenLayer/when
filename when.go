912726890602c769551ff34e7d643c6ec7bcc12c
astrlux <37908451+astrlux@users.noreply.github.com>
2023-09-02 02:00:34 -0700

    format files

--- when.go
+++ when.go
@@ -1,26 +1,25 @@
 package when
 
 import (
-    "fmt"
+	"fmt"
 
-    p "github.com/ubhattac/when/parser"
-    d "github.com/ubhattac/when/description"
+	d "github.com/ubhattac/when/description"
+	p "github.com/ubhattac/when/parser"
 )
 
-func When(input string) (string, error) {
-    // Try parsing using RFC3339 parser
-    t, err := p.ParseRFC3339(input)
-    if err == nil {
-        return d.GenerateDescription(t), nil
-    }
+func When(time string) (string, error) {
+	// Try parsing using RFC3339 parser
+	t, err := p.ParseRFC3339(time)
+	if err == nil {
+		return d.GenerateDescription(t), nil
+	}
 
-    // Try parsing using Unix timestamp parser
-    t, err = p.ParseUnixTime(input)
-    if err == nil {
-        return d.GenerateDescription(t), nil
-    }
+	// Try parsing using Unix timestamp parser
+	t, err = p.ParseUnixTime(time)
+	if err == nil {
+		return d.GenerateDescription(t), nil
+	}
 
-    // If parsing using all parsers fails, return an error
-    return "", fmt.Errorf("unable to parse timestamp")
+	// If parsing using all parsers fails, return an error
+	return "", fmt.Errorf("unable to parse timestamp")
 }
-
