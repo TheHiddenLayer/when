dcc0877487f827c13072482e13ad60799ab1d621
astrlux <37908451+astrlux@users.noreply.github.com>
2023-09-02 03:00:12 -0700

    define constants for durations of time

---
+++ description/constants.go
@@ -0,0 +1,11 @@
+package description
+
+const (
+	second = 1
+	minute = 60 * second
+	hour   = 60 * minute
+	day    = 24 * hour
+	week   = 7 * day
+	month  = (4 * week) + (2 * day)
+	year   = 12 * month
+)
